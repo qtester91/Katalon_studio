24c26be31b5be8c86eec3024dc20f799be8c658b
bojana <bojana@wedoqa.co>
2019-10-25 15:48:44 +0200

    Initial set of test cases for Dutchie

---
+++ Object Repository/Dutchie/AddToCart.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>AddToCart</name>
+   <tag></tag>
+   <elementGuidId>a42ba189-660f-4dc4-a640-bd5448163978</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[contains(text(), 'ADD TO CART - $')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
